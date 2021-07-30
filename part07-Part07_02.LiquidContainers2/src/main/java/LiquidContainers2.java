7b402d99da09522748894133e8b09ebff13375d9
Tushar-Laptop <reddygtvs@gmail.com>
2021-07-30 22:28:42 +0530

    part i (7-2) check

--- part07-Part07_02.LiquidContainers2/src/main/java/LiquidContainers2.java
+++ part07-Part07_02.LiquidContainers2/src/main/java/LiquidContainers2.java
@@ -4,15 +4,35 @@ import java.util.Scanner;
 public class LiquidContainers2 {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
+        Scanner scanner = new Scanner(System.in);
+        Container first = new Container();
+        Container second = new Container();
 
         while (true) {
-            System.out.print("> ");
+            System.out.println("First: " + first);
+            System.out.println("Second: " + second);
+            //System.out.print("> ");
 
-            String input = scan.nextLine();
-            if (input.equals("quit")) {
+            String spl = scanner.nextLine();
+            if (spl.equals("quit")) {
                 break;
+            }
+            String[] parts = spl.split(" ");
+            String input = parts[0];
+            int amount = Integer.valueOf(parts[1]);
+            if (input.equals("add") && amount > 0) {
+                first.add(amount);
+            } else if (input.equals("move") && amount > 0) {
+                if (first.contains() <= amount) {
+                    second.add(first.contains());
+                    first.remove(amount);
+                } else {
+                    second.add(amount);
+                    first.remove(amount);
+                }
+                
+            } else if (input.equals("remove") && amount > 0) {
+                second.remove(amount);
             }
 
         }
